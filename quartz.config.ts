34a827e46859f26aec4efd4477a52a2e6fbf87b1
SMAnatoly <44983392+SMAnatoly@users.noreply.github.com>
2025-05-16 15:17:28 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Мои проекты",
+    pageTitle: "Smirnov Anatoly",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
